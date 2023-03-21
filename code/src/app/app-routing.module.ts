6cd274d894f9a5a5da64f6ecc1b53ea30c98e4d4
Randy-Bruce-LY <randy-bruce.ly@klocel.com>
2023-03-21 18:17:03 +0100

    Advanced

--- code/src/app/app-routing.module.ts
+++ code/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUserComponent } from './add-user/add-user.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 
 const routes: Routes = [
   {path: 'connexion', component: ConnexionComponent},
+  {path: 'add-user', component: AddUserComponent},
 ];
 
 @NgModule({
--- code/src/app/app.module.ts
+++ code/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnexionComponent } from './connexion/connexion.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConnexionComponent
+    ConnexionComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule,
