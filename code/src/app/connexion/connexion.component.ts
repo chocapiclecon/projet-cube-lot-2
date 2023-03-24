c44bcc2c07bf60eb5efd0eb84fa8037a932a122e
Randy-Bruce-LY <randybruceecole@gmail.com>
2023-03-24 14:19:16 +0100

    Adjustment

--- code/src/app/connexion/connexion.component.ts
+++ code/src/app/connexion/connexion.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-connexion',
@@ -15,11 +16,13 @@ export class ConnexionComponent {
 
   constructor(
     private formBuilder: FormBuilder,
+    private router: Router,
   ) {}
 
   onSubmit(): void {
     // Process checkout data here
     console.warn('Your order has been submitted', this.loginForm.value);
+    // this.router.navigate(['/acceuil']);
     this.loginForm.reset();
   }
 
