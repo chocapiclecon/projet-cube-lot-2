32a1345a5387b608d00f2bf8d9a7f27f1ee73df9
DukMik <dukmik.game@gmail.com>
2023-03-24 09:44:16 +0100

    page d'acceuil
    page d'accueil

--- code/src/acceuil/acceuil.component.spec.ts
+++ code/src/acceuil/acceuil.component.spec.ts
@@ -2,34 +2,34 @@
 import { TestBed } from '@angular/core/testing';
 // @ts-ignore
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { acceuilComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('acceuilComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        acceuilComponent
       ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(acceuilComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'code'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(acceuilComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('code');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(acceuilComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('code app is running!');
---
+++ code/src/acceuil/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+// @ts-ignore
+@Component({
+  selector: 'app-root',
+  templateUrl: './acceuil.component.html',
+  styleUrls: ['./acceuil.component.css']
+})
+export class acceuilComponent {
+  title = 'code';
+  Titre: string  = "houhou";
+  AcceuilCliquable() {
+
+  }
+  ConnectionCliquable() {
+
+  }
+
+}
+
